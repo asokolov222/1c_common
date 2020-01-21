ad22d06111c409cc89e40ef2c41b53590a9465ea
@asokolov222 <programmer.2229923@gmail.com>
2020-01-21 23:37:52 +0300

    Изменили шаблон

---
+++ reg64.bat
@@ -0,0 +1,11 @@
+rem regsvr32 /u "C:\Program Files\1cv8\8.3.13.1513\bin\comcntr.dll"
+
+rem regsvr32 /u "C:\Program Files\1cv8\8.3.16.1030\bin\comcntr.dll"
+
+
+rem regsvr32 /u "C:\Program Files\1cv8\8.3.15.1489\bin\comcntr.dll"
+rem C:\Windows\SysWOW64\regsvr32 /u "C:\Program Files\1cv8\8.3.15.1489\bin\comcntr.dll"
+C:\Windows\SysWOW64\regsvr32 /i "C:\Program Files\1cv8\8.3.15.1489\bin\comcntr.dll"
+
+
+
