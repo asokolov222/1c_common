a3f73ee1e7f8a7542e6b71ef1695f1ca4c3b148f
@asokolov222 <programmer.2229923@gmail.com>
2021-05-31 23:28:14 +0300

    11

--- reg64.bat
+++ reg64.bat
@@ -1,11 +1,4 @@
-rem regsvr32 /u "C:\Program Files\1cv8\8.3.13.1513\bin\comcntr.dll"
-
-rem regsvr32 /u "C:\Program Files\1cv8\8.3.16.1030\bin\comcntr.dll"
-
-
-rem regsvr32 /u "C:\Program Files\1cv8\8.3.15.1489\bin\comcntr.dll"
-rem C:\Windows\SysWOW64\regsvr32 /u "C:\Program Files\1cv8\8.3.15.1489\bin\comcntr.dll"
-C:\Windows\SysWOW64\regsvr32 /i "C:\Program Files\1cv8\8.3.18.1289\bin\comcntr.dll"
+C:\Windows\SysWOW64\regsvr32 /i "C:\Program Files\1cv8\8.3.18.1363\bin\comcntr.dll"
 
 
 
